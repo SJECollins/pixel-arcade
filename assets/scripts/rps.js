f77f6dc62fee138584a899e8e14e69d9b6355abb
SCollins <jaybuckco@gmail.com>
2022-06-14 14:31:19 +0000

    Add comments to rps.js code so easier to follow

--- assets/scripts/rps.js
+++ assets/scripts/rps.js
@@ -13,9 +13,7 @@ const threeRounds = document.getElementById("three-rounds")
 const fiveRounds = document.getElementById("five-rounds")
 const roundText = document.getElementById("round-text")
 
-threeRounds.addEventListener("click", playThree)
-fiveRounds.addEventListener("click", playFive)
-
+// Our global variables
 let computerPick
 let userPick
 let result
@@ -24,6 +22,7 @@ let computerPoints = 0
 let round = 0
 let numberOfRounds = 3
 
+// Our functions to select our levels
 function playThree() {
   threeRounds.removeEventListener("click", playThree)
   fiveRounds.removeEventListener("click", playFive)
@@ -40,6 +39,10 @@ function playFive() {
   roundText.innerHTML = "Five"
 }
 
+/**
+ * We use the forEach method set the userPick variable from the ID of the image they selected
+ * And then call userChoice to display an image, and call computerChoice and compare
+ */
 choices.forEach(choice => choice.addEventListener("click", (event) => {
   userPick = event.target.id
   userChoice()
@@ -59,6 +62,13 @@ function userChoice() {
   }
 }
 
+/**
+ * The computerChoice function randomly picks a number and then assigns the computerPick
+ * and displays it's choice
+ * We could hardcode 3 for choices.length as we're only deciding between 3 options, but
+ * if we decide to add an option for the user to select between classic RPS and Rock, Paper,
+ * Scissors, Lizard, Spock or another varient, using a global variable makes it more dynamic
+ */
 function computerChoice() {
   const randomChoice = Math.floor(Math.random() * choices.length)
   if (randomChoice === 0) {
@@ -75,6 +85,11 @@ function computerChoice() {
   }
 }
 
+/**
+ * Our compare function uses else if to compare the picks, increment points and update
+ * our round results
+ * It also increments the rounds, displays results and calls endGame to check for a winner
+ */
 function compare() {
   if (computerPick === "rock" && userPick === "paper") {
     userPoints++
@@ -100,6 +115,12 @@ function compare() {
   userScore.innerHTML = userPoints
 }
 
+/**
+ * Our endGame function compares the user and computer's point tallies to the number of
+ * rounds to determine the winner, but returns out of itself if no one has met the
+ * requirement
+ * numberOfRounds can be set by our earlier level functions
+ */
 function endGame() {
   if (computerPoints === numberOfRounds) {
     endScore.innerHTML = "YOU LOST"
@@ -112,6 +133,9 @@ function endGame() {
   }
 }
 
+// Our EventListeners
+threeRounds.addEventListener("click", playThree)
+fiveRounds.addEventListener("click", playFive)
 reset.addEventListener("click", () => {
   location.reload() 
 })
